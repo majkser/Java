9517f0e102ed3b48572c0d968e9bb74cf9bd3a58
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-10-21 01:11:20 +0200

    initail

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+public class Main {
+    public static void main(String[] args) {
+        Zadanie01 zadanie = new Zadanie01();
+
+        //a
+        zadanie.input(0);
+        zadanie.input(1);
+//        int a = zadanie.wynik();
+//        System.out.println(a);
+        zadanie.input(0);
+        zadanie.input(1);
+        //a
+
+        //b
+        zadanie.input(1);
+        zadanie.input(1);
+        zadanie.input(1);
+        zadanie.input(1);
+
+        zadanie.input(1);
+        //b
+
+        //c
+        zadanie.input(0);
+
+        zadanie.input(0);
+
+        //c
+
+        //d
+        zadanie.input(0);
+        zadanie.input(1);
+        zadanie.input(1);
+
+        zadanie.input(1);
+        zadanie.input(0);
+        //d
+
+//        a = zadanie.wynik();
+//        System.out.println(a);
+
+        ////////////////////////////
+
+        //a
+        zadanie.input(0);
+        zadanie.input(1);
+
+        zadanie.input(0);
+        zadanie.input(1);
+        //a
+
+        //b
+        zadanie.input(0);
+        zadanie.input(1);
+        zadanie.input(1);
+        zadanie.input(1);
+
+        zadanie.input(1);
+        //b
+
+        //c
+        zadanie.input(0);
+        zadanie.input(0);
+
+        //c
+
+        //d
+        zadanie.input(0);
+        zadanie.input(1);
+        zadanie.input(1);
+        zadanie.input(1);
+        zadanie.input(0);
+        //d
+
+//
+//        a = zadanie.wynik();
+//        System.out.println(a);
+
+
+    }
+}
